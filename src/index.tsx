63ebf784f5af4508d4efb4c06bef190499fa53d3
Elliot Nash <elliotnash@gmail.com>
2021-05-27 17:19:07 -0700

    move styling into index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CSS from 'csstype';
 import './index.css';
 import Map from './map';
 import reportWebVitals from './reportWebVitals';
 
-const background: CSS.Properties = {
-  position: "absolute",
-  display: "flex",
-  backgroundColor: "DodgerBlue",
-  top: "0",
-  bottom: "0",
-  left: "0",
-  right: "0",
-};
-
 ReactDOM.render(
   <React.StrictMode>
-    <div style={background}>
+    <div className="background">
       <Map/>
-    </div>, 
+    </div>,
   </React.StrictMode>,
   document.getElementById('root')
 );
